1eefb1365a374a5412071fc3d918a607c561db0c
Valentin Brosseau <c4software@gmail.com>
2023-07-09 15:34:18 +0200

    Lecture de la configuration dans le home directory de l'utilisateur

--- src/jvmMain/kotlin/data/env/RepositoryEnv.kt
+++ src/jvmMain/kotlin/data/env/RepositoryEnv.kt
@@ -7,9 +7,18 @@ import java.io.File
 object RepositoryEnv {
 
     fun readConfigurationFile(): Configuration {
-       return try {
-            val configuration = File("configuration.yaml").readText()
-            return Yaml.decodeFromString(Configuration.serializer(), configuration)
+        return try {
+            val path = File(System.getProperty("user.home") + "/.damp/configuration.yaml")
+            val localPath = File("configuration.yaml")
+            if (localPath.exists()) {
+                val configuration = localPath.readText()
+                return Yaml.decodeFromString(Configuration.serializer(), configuration)
+            } else if (path.exists()) {
+                val configuration = path.readText()
+                return Yaml.decodeFromString(Configuration.serializer(), configuration)
+            } else {
+                Configuration()
+            }
         } catch (e: Exception) {
             print(e)
             Configuration()
